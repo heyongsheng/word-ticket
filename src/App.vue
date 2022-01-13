38500c08097a0f5dfe843262a75d6c59ac92de38
贺永胜 <1378431028@qq.com>
2022-01-13 12:19:12 +0800

    增加音效控制

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,22 @@
  * @Date: 2022-01-12 15:31:53
  * @email: 1378431028@qq.com
  * @LastEditors: 贺永胜
- * @LastEditTime: 2022-01-13 01:06:01
+ * @LastEditTime: 2022-01-13 11:59:54
  * @Descripttion: 游戏首页
 -->
 <template>
   <div id="app">
+    <!-- 文章链接 -->
+    <a class="help-btn audio-btn" target="_blank" href="https://baidu.com">?</a>
+    <!-- 声音控制 -->
+    <div class="audio-btn" :class="{active: audioState}" @click="audioState = !audioState">♫</div>
+
     <!-- 游戏介绍 -->
     <div class="panel-class" v-show="gameStatus === 'desc'">
-      <div class="panel-text">赶车啦！</div>
+      <div class="panel-text">青山抢票</div>
       <div class="panel-desc">
-        高铁还有一分钟停止检票，你距离高铁站还有5000米，打出一个单词可以瞬移100米，拼手速时刻到了，快来试试你能否在规定时刻到达高铁站！输入 abandon 开始游戏
+        又到春节了，你归心似箭，但还没买到票，青山抢票推出敲单词加速抢票服务，输入正确50个单词才能买票成功，拼手速的时候到了，快冲！<br>
+        输入 abandon 开始游戏
       </div>
     </div>
     <!-- 游戏区 -->
@@ -21,16 +27,16 @@
       <div class="progress-wrap">
         <!-- 剩余发车时间 -->
         <div class="progress">
-          <p class="progress-text">剩余发车时间：{{currentTime}}s</p>
-          <div class="progress-inside" :style="{width: currentTime / time * 100 + '%'}">
-            <p class="progress-text">剩余发车时间：{{currentTime}}s</p>
+          <p class="progress-text">余票：{{currentTicketCount}}</p>
+          <div class="progress-inside" :style="{width: currentTicketCount / ticketCount * 100 + '%'}">
+            <p class="progress-text">余票：{{currentTicketCount}}</p>
           </div>
         </div>
         <!-- 距离车站距离 -->
         <div class="progress">
-          <p class="progress-text">距离车站还有：{{currentDistance}}m</p>
-          <div class="progress-inside" :style="{width: currentDistance / distance * 100 + '%'}">
-            <p class="progress-text">距离车站还有：{{currentDistance}}m</p>
+          <p class="progress-text">剩余验证码：{{currentCodeCount}}</p>
+          <div class="progress-inside" :style="{width: currentCodeCount / codeCount * 100 + '%'}">
+            <p class="progress-text">剩余验证码：{{currentCodeCount}}</p>
           </div>
         </div>
       </div>
@@ -44,13 +50,13 @@
     <div class="panel-class" v-show="gameStatus === 'end'">
       <!-- 结果 -->
       <div class="panel-text">
-        <p v-show="result === 'success'">成功上车！</p>
-        <p v-show="result === 'fail'">上车失败！</p>
+        <p v-show="result === 'success'">抢票成功！</p>
+        <p v-show="result === 'fail'">票已售罄！</p>
       </div>
       <!-- 战绩 -->
       <div class="panel-desc">
-        <p v-show="result === 'success'">经过不懈的努力，您在第 <b>{{round}}</b> 轮游戏中就成功上车了，相信什么话语都无法描述您此刻想回家的心情，青山科技在此祝您的旅途一路顺风，同时也祝您身体健康，阖家欢乐，团团圆圆过大家</p>
-        <p v-show="result === 'fail'">很遗憾您没赶上车，不过青山科技本着帮人帮到底的诚意，给您提供了第 <b>{{round + 1}}</b> 次机会，你可以输入 <b>again</b> 穿越到 <b>{{time+5}}</b> 秒前，再来一次，希望这次您能成功！加油！</p>
+        <p v-show="result === 'success'">经过不懈的努力，您在第 <b>{{round}}</b> 轮游戏中就抢到票了，相信什么话语都无法描述您此刻想回家的心情，青山科技在此祝您的旅途一路顺风，同时也祝您身体健康，阖家欢乐，团团圆圆过大年</p>
+        <p v-show="result === 'fail'">抢票失败！不过第 <b>{{round + 1}}</b> 轮放票来了，本轮票量增加5张，你可以输入 <b>again</b> 参与本轮抢票，希望这次您能成功！加油！</p>
       </div>
     </div>
     <!-- 输入框 -->
@@ -64,20 +70,37 @@ export default {
   name: 'App',
   data () {
     return {
+      dingMedia: require('@/assets/audio/ding.wav'),
+      successMedia: require('@/assets/audio/success.mp3'),
+      failMedia: require('@/assets/audio/fail.mp3'),
+      audioState: false,
+      bgAudio: new Audio(require('@/assets/audio/bg.mp3')),
       round: 0,
-      time: 60, // 剩余发车时间
-      currentTime: 0, // 当前时间
-      distance: 5000,
-      currentDistance: 0,
+      ticketCount: 60, // 本轮票量
+      currentTicketCount: 0, // 当前票量
+      codeCount: 50, // 总验证码数
+      currentCodeCount: 0, // 剩余验证码数量
       wordInput: '', // 输入框的值
       wordLibrary: require('@/assets/data/word.json'), // 单词库
       currentWordLibrary: [], // 当前单词库
       currentWord: {}, // 当前单词
       gameStatus: 'desc', // 游戏状态
-      result: ''
+      result: '',
+      ticketCheckInterval: null // 检票定时器
+    }
+  },
+  watch: {
+    audioState (val) {
+      if (val) {
+        this.bgAudio.play()
+      } else {
+        this.bgAudio.pause()
+      }
     }
   },
   mounted () {
+    // 设置背景音乐属性
+    this.bgAudio.loop = true
     // this.startGame()
   },
   methods: {
@@ -87,6 +110,10 @@ export default {
      * @return {*}
      */
     startGame () {
+      // 如果声音开关打开，则播放背景音乐
+      if (this.audioState) {
+        this.bgAudio.play()
+      }
       // 改变游戏状态
       this.gameStatus = 'start'
       // 游戏轮数加一
@@ -94,13 +121,13 @@ export default {
       // 重置本轮游戏词库
       this.currentWordLibrary = [...this.wordLibrary]
       // 根据轮数设置游戏时间
-      this.time = this.time + (this.round - 1 ) * 5
+      this.ticketCount = this.ticketCount + (this.round - 1 ) * 5
       // 重置时间
-      this.currentTime = this.time
+      this.currentTicketCount = this.ticketCount
       // 清空输入框
       this.wordInput = ''
       // 重置距离
-      this.currentDistance = this.distance
+      this.currentCodeCount = this.codeCount
       // 随机获取一个单词
       this.drawWord()
       // 开始检票
@@ -111,16 +138,13 @@ export default {
      * @param {*}
      * @return {*}
      */
-    ticketCheck () {
-      if (this.currentTime > 0) {
-        setTimeout(() => {
-          this.currentTime-=10
-          this.ticketCheck()
-        }, 1000)
-      } else {
-        // 游戏结束
-        this.gameOver()
-      }
+    ticketCheck () {   
+      this.ticketCheckInterval = setInterval(() => {
+        this.currentTicketCount--
+        if (this.currentTicketCount <= 0) {
+          this.gameOver()
+        }
+      }, 1000)
     },
     /**
      * @description: 抽取单词
@@ -151,11 +175,13 @@ export default {
       }
       // 判断是否答对
       if (this.wordInput === this.currentWord.word) {
+        // 播放音效
+        this.playAudio(this.dingMedia)
         // 重置输入框
         this.wordInput = ''
-        // 距离减100
-        this.currentDistance -= 100
-        if (this.currentDistance === 0) {
+        // 验证码数量减一
+        this.currentCodeCount-= 10
+        if (this.currentCodeCount <= 0) {
           this.gameOver()
           return
         }
@@ -169,16 +195,35 @@ export default {
      * @return {*}
      */    
     gameOver () {
-      // this.$refs.wordInput.blur()
+      if ((navigator.userAgent.match(/(iPhone|iPod|Android|ios|iOS|iPad|Backerry|WebOS|Symbian|Windows Phone|Phone)/i))) {
+        this.$refs.wordInput.blur()
+      }
+      // 暂停背景音乐
+      this.bgAudio.pause()
+      // 清楚检票定时器
+      clearInterval(this.ticketCheckInterval)
       // 判断胜负
-      if (this.currentTime > 0) {
-        // 胜利
+      if (this.currentTicketCount > 0) {
+        // 更改胜利状态
         this.result = 'success'
+        // 播放胜利音效
+        this.playAudio(this.successMedia)
       } else {
         // 失败
         this.result = 'fail'
+        // 播放失败音效
+        this.playAudio(this.failMedia)
       }
       this.gameStatus = 'end'
+    },
+    playAudio (src) {
+      if (this.audioState) {
+        const audio = new Audio()
+        audio.src = src
+        audio.load()
+        audio.volume = .5
+        audio.play()
+      }
     }
   }
 }
@@ -199,6 +244,38 @@ export default {
   background-position: center;
   background-size: cover;
   font-family: PingFangSC-Regular, Microsoft Yahei, sans-serif;
+}
+/* 声音控制 */
+.audio-btn {
+  color: #fff;
+  font-size: 30px;
+  width: 50px;
+  height: 50px;
+  border: 2px solid #fff;
+  line-height: 50px;
+  text-align: center;
+  border-radius: 50%;
+  position: absolute;
+  right: 20px;
+  top: 20px;
+  cursor: pointer;
+  user-select: none;
+}
+.audio-btn.active {
+  animation: spin 2s linear infinite;
+}
+/* 帮助中心 */
+.help-btn {
+  right: 90px;
+  text-decoration: none;
+}
+@keyframes spin {
+    from {
+        transform: rotate(0deg);
+    }
+    to {
+        transform: rotate(360deg);
+    }
 }
 /* 进度区 */
 .progress-wrap {
@@ -289,11 +366,11 @@ export default {
   font-size: 50px;
   line-height: 80px;
   top: -80px;
-  color: #f7f74b;
+  color: #ffff71;
   font-weight: bold;
   font-style: italic;
   text-align: center;
-  text-shadow: 2px 2px 1px #ea7b7b;
+  text-shadow: 2px 2px 2px #ff1d1d;
 }
 .panel-desc {
   font-size: 20px;
---
+++ vue.config.js
@@ -0,0 +1,13 @@
+/*
+ * @Author: 贺永胜
+ * @Date: 2022-01-13 10:07:53
+ * @email: 1378431028@qq.com
+ * @LastEditors: 贺永胜
+ * @LastEditTime: 2022-01-13 10:48:59
+ * @Descripttion: 
+ */
+
+module.exports = {
+  publicPath: "/word-ticket/", //打包后部署在一个子路径上http:xxx/m/
+  productionSourceMap: false
+};
